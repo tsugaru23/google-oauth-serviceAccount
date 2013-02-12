708d70be36a800cd984e6c8be6a4b3bafcdb0ea9
23 Tsugaru <tsugaru23@gmail.com>
2013-02-12 18:33:10 +0900

    list event example

---
+++ listEvents.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const gaccount = require('./google-serviceaccount');
+
+const calRoot = "https://www.googleapis.com/calendar/v3";
+
+gaccount.auth(function(err, access_token){
+
+	var token = "?access_token="+access_token;
+
+    request.get({
+        url:calRoot+"/users/me/calendarList"+token,
+        json:true
+        }, function(err, res, body){
+
+            for(var c=0; c<body.items.length; c++){
+                console.log(body.items[c]);
+
+                request.get({
+                    url:calRoot+"/calendars/"+body.items[c].id+"/events"+token,
+                    json:true
+                    }, function(err, res, cal){
+                        console.log(cal);
+                });
+            }
+    })
+	
+});
