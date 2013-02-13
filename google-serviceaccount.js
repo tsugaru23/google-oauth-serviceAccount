92ab8a4c9622dbbf259a7d84da19844f95c9fc27
OpenShift guest <tsugaru23@gmail.com>
2013-02-12 21:31:45 -0500

    refined with logging

--- google-serviceaccount.js
+++ google-serviceaccount.js
@@ -1,89 +1,108 @@
 process.env.TZ = 'UTC';
-//console.log(new Date().toString());
 const request = require('request');
 const crypto = require('crypto');
 const config = require('nconf');
 const fs = require('fs');
 
-var auth = function(callback){
+const logger = require('./lib/logger');
+logger.debug(new Date().toString());
 
-config.argv()
-	.env()
-	.file({file: 'config.json'});
-config.defaults({
-	keyFile: "key.pem"
-	,expiresInMinutes: 60
-});
-var keyFile = config.get('keyFile');
+var auth = function(callback) {
 
-var jwtHeader = {alg:"RS256",typ:"JWT"};
-var jwtHeaderB64 = base64urlEncode(JSON.stringify(jwtHeader));
-//console.log("Header:\n" + jwtHeaderB64);
-//decode
-//console.log(new Buffer(jwtHeaderB64, 'base64').toString('ascii'));
-
-// https://developers.google.com/google-apps/calendar/v3/reference/calendars/get
-var iat = Math.floor(new Date().getTime() / 1000);
-var exp = iat + (config.get('expiresInMinutes') * 60);
-
-var jwtClaim = config.get('claim');
-jwtClaim.exp = exp;
-jwtClaim.iat = iat;
-
-//console.log(JSON.stringify(jwtClaim));
-var jwtClaimB64 = base64urlEncode(JSON.stringify(jwtClaim));
-//console.log("Claim:\n" + jwtClaimB64);
-
-var signatureInput = jwtHeaderB64 + '.' + jwtClaimB64;
-//console.log("Signature Input:\n" + signatureInput);
-
-var signature = sign(signatureInput, fs.readFileSync(keyFile));
-//console.log("Signature:\n" + signature);
-
-var JWT = signatureInput + '.' + signature;
-//console.log("JWT:\n"+JWT);
-
-request.post({
-    url: 'https://accounts.google.com/o/oauth2/token',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    form:{
-        grant_type:'urn:ietf:params:oauth:grant-type:jwt-bearer',
-        assertion:JWT
-    }
-}, function(err, res, body) {
-    if(err) throw err;
-
-    if(res.statusCode != 200){
-    	console.log('STATUS: ' + res.statusCode);
-    	//console.log('HEADERS: ' + JSON.stringify(res.headers));
-
-    	console.log('Response:\n' + body);
-	throw "failed to retrieve an access token";
+    config.argv().env().file({
+        file: 'config.json'
+    });
+    config.defaults({
+        keyFile: "key.pem",
+        expiresInMinutes: 60
+    });
+    var keyFile = config.get('keyFile');
+    if(!fs.existsSync(keyFile)){
+        logger.error("keyFile not found:"+keyFile);
+        process.exit(1);
     }
 
-    callback(err, JSON.parse(body).access_token);
-});
+    var jwtHeader = {
+        alg: "RS256",
+        typ: "JWT"
+    };
+    var jwtHeaderB64 = base64urlEncode(JSON.stringify(jwtHeader));
+    logger.debug("Header:" + jwtHeaderB64);
+    //decode
+    logger.debug(new Buffer(jwtHeaderB64, 'base64').toString('ascii'));
+
+    // https://developers.google.com/google-apps/calendar/v3/reference/calendars/get
+    var iat = Math.floor(new Date().getTime() / 1000);
+    var exp = iat + (config.get('expiresInMinutes') * 60);
+
+    var jwtClaim = config.get('claim');
+    jwtClaim.exp = exp;
+    jwtClaim.iat = iat;
+
+    logger.debug(JSON.stringify(jwtClaim));
+    var jwtClaimB64 = base64urlEncode(JSON.stringify(jwtClaim));
+    logger.debug("Claim:" + jwtClaimB64);
+
+    var signatureInput = jwtHeaderB64 + '.' + jwtClaimB64;
+    logger.debug("Signature Input:" + signatureInput);
+
+    var JWT = null;
+    var signature = sign(signatureInput, keyFile);
+    logger.debug("Signature:" + signature);
+
+    JWT = signatureInput + '.' + signature;
+    logger.debug("JWT:"+JWT);
+
+    request.post({
+        url: 'https://accounts.google.com/o/oauth2/token',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        form: {
+            grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            assertion: JWT
+        }
+    }, function(err, res, body) {
+        if (err){
+            callback(err, null);
+        }else{
+            if (res.statusCode == 200) {
+                logger.info("STATUS:200");
+                callback(err, JSON.parse(body).access_token);
+            }else{
+                logger.warning('STATUS: ' + res.statusCode);
+                logger.debug('HEADERS: ' + JSON.stringify(res.headers));
+                logger.error('Response:\n' + body);
+
+                callback(new Error("failed to retrieve an access token"), body);
+            }
+        }
+    });
 
 }
 
-function sign(inStr, key){
+function sign(inStr, keyPath) {
 
-    var sig = crypto.createSign('RSA-SHA256')
-        .update(inStr)
-        .sign(key, 'base64');
+    var key = fs.readFileSync(keyPath);
+    if(key.length==0)
+        logger.warning("most likely invalid key file: " + keyPath);
 
-   //verification
+    var sig = crypto.createSign('RSA-SHA256').update(inStr).sign(key, 'base64');
+
+    //verification
     var verifier = crypto.createVerify("RSA-SHA256");
     verifier.update(inStr);
-    //console.log("verify:" + verifier.verify(key, sig, 'base64'));
+    if(verifier.verify(key, sig, 'base64')){
+        logger.debug("signature verified with:"+keyPath);
+    }else{
+        logger.error("signature NOT verified with:"+keyPath);
+    }
 
     return base64urlEscape(sig);
 }
 
 function base64urlEncode(str) {
-  return base64urlEscape(new Buffer(str).toString('base64'));
+    return base64urlEscape(new Buffer(str).toString('base64'));
 }
 
 function base64urlEscape(str) {
--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const gaccount = require('./google-serviceaccount');
 
 gaccount.auth(function(err, access_token){
+    if(err)throw err;
+
 	console.log('token:'+access_token);
 });
